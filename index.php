ff9b5d2bfa1623171d3be39beac1040c4e474b92
ajadi.paul <ajadi.paul@e4email.net>
2020-06-18 17:34:39 +0100

    make it better

--- index.php
+++ index.php
@@ -43,6 +43,7 @@ if (isset($_POST['get_airtime'])) {
             "headers": {
                 "Content-Type": "application/json",
                 "X-Requested-With": "XMLHttpRequest",
+                "Access-Control-Allow-Origin": "*",
             },
             "data": JSON.stringify({"Code":"airtel","Amount":100,"PhoneNumber":"07068260000","SecretKey":"60m1fdz9shbd "}),
             };
