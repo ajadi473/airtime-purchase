9358889626af8695a25769d9ea50f152e000d8a0
ajadi.paul <ajadi.paul@e4email.net>
2020-06-19 13:58:28 +0100

    added html

--- index.php
+++ index.php
@@ -39,19 +39,53 @@ if (isset($_POST['get_airtime'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Send Airtime - HNG Internship Entry</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 </head>
 <body>
-    <form action="" method="post">
-        <input type="number" name="number" placeholder="Provide Number" required>
-        <input type="number" name="amount" size="4" id="provice Amount" required>
-        <select value="" name="network">
-            <option selected name="mtn" id="">MTN</option>
-            <option name="airtel" id="">Airtel</option>
-            <option name="glo" id="">GLO</option>
-        </select>
-        <input type="submit" name="get_airtime" value="Send Airtime">
-    </form>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-10 col-xl-9 mx-auto">
+            <div class="card card-signin flex-row my-5">
+            <div class="card-img-left d-none d-md-flex">
+                <!-- Background image for card set in CSS! -->
+            </div>
+            <div class="card-body">
+                <h5 class="card-title text-center">Welcome, SMS SENDER</h5>
+                <form class="form-signin" method="POST">
+                <div class="form-label-group">
+                    <input type="number" id="inputNumber" name="number" placeholder="Provide Number" class="form-control" required autofocus>
+                    <label for="inputNumber">Provide Mobile Number to send sms to</label>
+                </div>
 
+                <div class="form-label-group">
+                    <input type="number" id="inputAmount" name="amount" class="form-control" placeholder="Send Amount" required>
+                    <label for="inputAmount">Amount to send</label>
+                </div>
+                
+                <hr>
+
+                <div class="form-label-group">
+                    <select id="inputNetwork" class="form-control" name="network" required>
+                        <option selected name="mtn" id="">MTN</option>
+                        <option name="airtel" id="">Airtel</option>
+                        <option name="glo" id="">GLO</option>
+                    </select>
+                    
+                </div>
+
+                <button name="get_airtime" class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Initiate Transaction</button>
+                <hr class="my-4">
+                </form>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+    <hr>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
